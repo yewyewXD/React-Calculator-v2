b3a044cb8d84efa3c05a81220f797fd7627cd229
Yew Kang Wei <yewyew6933@gmail.com>
2020-06-05 16:00:53 +0800

    on-click number

--- src/App.js
+++ src/App.js
@@ -1,29 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [numbers, setNumbers] = useState([]);
+  const [num, setNum] = useState("");
+
+  function onClickNum(e) {
+    setNum(e.target.innerHTML);
+    const newNumList = [...numbers, e.target.innerHTML];
+    setNumbers(newNumList);
+  }
+
   return (
     <div>
       <div className="container">
         <h1 className="heading">React Calculator</h1>
-
+        <div className="numpad-head">{numbers}</div>
         <div className="numpad">
           <div className="num-row">
-            <div className="num">1</div>
-            <div className="num">2</div>
-            <div className="num">3</div>
+            <div className="num" onClick={onClickNum}>
+              1
+            </div>
+            <div className="num" onClick={onClickNum}>
+              2
+            </div>
+            <div className="num" onClick={onClickNum}>
+              3
+            </div>
           </div>
 
           <div className="num-row">
-            <div className="num">4</div>
-            <div className="num">5</div>
-            <div className="num">6</div>
+            <div className="num" onClick={onClickNum}>
+              4
+            </div>
+            <div className="num" onClick={onClickNum}>
+              5
+            </div>
+            <div className="num" onClick={onClickNum}>
+              6
+            </div>
           </div>
 
           <div className="num-row">
-            <div className="num">7</div>
-            <div className="num">8</div>
-            <div className="num">9</div>
+            <div className="num" onClick={onClickNum}>
+              7
+            </div>
+            <div className="num" onClick={onClickNum}>
+              8
+            </div>
+            <div className="num" onClick={onClickNum}>
+              9
+            </div>
           </div>
         </div>
       </div>
