7d0f1a395871c604e3c57f5e3c5de8feb3f88750
Yew Kang Wei <yewyew6933@gmail.com>
2020-06-05 15:54:47 +0800

    numpad UI

--- src/App.js
+++ src/App.js
@@ -4,7 +4,29 @@ import "./App.css";
 function App() {
   return (
     <div>
-      <h1>Testing</h1>
+      <div className="container">
+        <h1 className="heading">React Calculator</h1>
+
+        <div className="numpad">
+          <div className="num-row">
+            <div className="num">1</div>
+            <div className="num">2</div>
+            <div className="num">3</div>
+          </div>
+
+          <div className="num-row">
+            <div className="num">4</div>
+            <div className="num">5</div>
+            <div className="num">6</div>
+          </div>
+
+          <div className="num-row">
+            <div className="num">7</div>
+            <div className="num">8</div>
+            <div className="num">9</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
